f1620b7077a0811cbbcea046b2c463667379d59b
moe3344 <mohamad.hammoud1@ucalgary.ca>
2023-11-23 18:00:37 -0700

    moe

--- Seat.java
+++ Seat.java
@@ -1,28 +1,38 @@
 public class Seat {
-    private String seatNumber;
-    private String seatType;
+    private double seatCost =70.0;
+    private int seatNumber;
+    private String seatLetter;
 
     // Constructor
-    public Seat(String seatNumber, String seatType) {
+    public Seat(int seatNumber, String seatType) {
         this.seatNumber = seatNumber;
-        this.seatType = seatType;
+        this.seatLetter = seatType;
     }
 
     // Getter methods
-    public String getSeatNumber() {
+    public int getSeatNumber() {
         return seatNumber;
     }
 
-    public String getSeatType() {
-        return seatType;
+    public String getSeatLetter() {
+        return seatLetter;
     }
 
     // Setter methods
-    public void setSeatNumber(String seatNumber) {
+    public void setSeatNumber(int seatNumber) {
         this.seatNumber = seatNumber;
     }
 
     public void setSeatType(String seatType) {
-        this.seatType = seatType;
+        this.seatLetter = seatType;
     }
-}
+    public double seatCost(){
+        if (seatNumber<=2){
+            return 2.1*seatCost;
+        }
+        else if (seatNumber>2 &&seatNumber<=6){
+            return (0.4 * seatCost);
+        }
+        
+        return seatCost;
+    }
